bbb91ca265a259a71818535d3ead0747a0b17b59
Soner Karabulut <84913012+BuzOkcu@users.noreply.github.com>
2023-02-09 20:48:17 +0300

    Add files via upload

---
+++ c01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skarabul <skarabul@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/09 14:55:49 by skarabul          #+#    #+#             */
+/*   Updated: 2023/02/09 14:55:56 by skarabul         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int		i;
+	int		swap;
+
+	while (size >= 0)
+	{
+		i = 0;
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				swap = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = swap;
+			}
+			i++;
+		}
+		size--;
+	}
+}
